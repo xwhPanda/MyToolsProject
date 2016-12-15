eeb1dd31eec90c4fa8a7f76bd59631f2dddf6174
panda <https://github.com/xwhPanda/MyToolsProject.git>
2016-12-15 09:20:22 +0800

    Rejected commit: Default

---
+++ NoteBook/src/main/java/xwh/com/bean/Author.java
@@ -0,0 +1,15 @@
+package xwh.com.bean;
+
+import java.io.Serializable;
+
+/**
+ * Author xwh
+ * Email 1152046774@qq.com
+ * CreateTime 2016/12/14 11:55
+ */
+public class Author implements Serializable{
+    /* 姓名 */
+    private String name;
+
+    private String url;
+}
